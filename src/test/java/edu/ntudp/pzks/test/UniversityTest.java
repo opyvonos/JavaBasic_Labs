87bc40dfd07061d10c5df8f27784ad747ec1d9b9
opyvonos <olyapivonos@gmail.com>
2025-03-25 09:27:03 +0200

    update UniversityTest.java

--- src/test/java/edu/ntudp/pzks/test/UniversityTest.java
+++ src/test/java/edu/ntudp/pzks/test/UniversityTest.java
@@ -5,7 +5,6 @@ import edu.ntudp.pzks.lab4.model.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UniversityTest {
     @Test
